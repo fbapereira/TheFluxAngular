30bb08aa66b460cfb6f349555cba40795d467a47
F. de Almeida Pereira <f.de.almeida.pereira@avanade.com>
2017-11-26 11:16:26 -0200

    Servicos

---
+++ src/servicos/login.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http/src/client';
+
+@Injectable()
+export class LoginService {
+
+    constructor(private http: HttpClient) {
+
+    }
+
+}

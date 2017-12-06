05544b5f5a200c3e76fa963e7c81105c614ee906
F. de Almeida Pereira <f.de.almeida.pereira@avanade.com>
2017-12-06 17:04:41 -0200

    Adiciona movimentacao

--- src/app/add-movimentacao-component/add-movimentacao.component.ts
+++ src/app/add-movimentacao-component/add-movimentacao.component.ts
@@ -5,6 +5,10 @@ import { ToasterService } from 'angular2-toaster';
 import { RouterModule, Router } from '@angular/router';
 import { UsuarioService } from '../../servicos/usuario.service';
 import { debounce } from 'rxjs/operator/debounce';
+import { TipoMovimentacaoService } from '../../servicos/tipo-movimentacao.service';
+import { TipoMovimentacao } from '../../modelos/tipo-movimentacao';
+import { TipoPagamentoService } from '../../servicos/tipo-pagamento.service';
+import { TipoPagamento } from '../../modelos/tipo-pagamento';
 
 @Component({
     selector: 'u2x-tf-usuario',
@@ -12,11 +16,30 @@ import { debounce } from 'rxjs/operator/debounce';
 })
 export class AddMovimentacaoComponent {
     oUsuario: Usuario;
+    id: number;
+
+    tipoMovimentacaos: TipoMovimentacao[] = [];
+    tipoPagamentos: TipoPagamento[] = [];
+
+
     constructor(
         private usuarioService: UsuarioService,
         private toasterService: ToasterService,
+        private tipoMovimentacaoService: TipoMovimentacaoService,
+        private tipoPagamentoService: TipoPagamentoService,
         private router: Router) {
         this.oUsuario = new Usuario();
+        this.id = this.usuarioService.usuario.instituicao.id;
+
+        this.tipoMovimentacaoService.Get(this.id)
+            .subscribe((tipoMovimentacaos: TipoMovimentacao[]) => {
+                this.tipoMovimentacaos = tipoMovimentacaos.filter((tipoMovimentacao: TipoMovimentacao) => { return !tipoMovimentacao.isCancelado });
+            });
+        this.tipoPagamentoService.Get(this.id)
+            .subscribe((tipoPagamento: TipoPagamento[]) => {
+                debugger;
+                this.tipoPagamentos = tipoPagamento.filter((tipoPagamento: TipoPagamento) => { return tipoPagamento.is_ativo });
+            });
     }
 
     btnAdd(usuario: Usuario): void {
